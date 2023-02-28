4e030e52d0cbe2dba6f7b1d7a129ebebdb7a0b84
Azka Nahya Amanta <mynameis.azkaa@gmail.com>
2023-02-28 23:08:59 +0700

    add category and update post

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -44,11 +44,11 @@ export default function PostPage({
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
         <header>
-          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
+          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-4">
             {frontMatter.title}
           </h1>
-          {frontMatter.description && (
-            <p className="text-xl mb-4">{frontMatter.description}</p>
+          {frontMatter.category && (
+            <p className="text-xl mb-12 font-semibold text-primary dark:text-primary text-center">{frontMatter.category}</p>
           )}
         </header>
         <main>
